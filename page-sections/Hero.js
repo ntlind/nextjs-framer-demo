2c12e7d43dc58f988e681972857cb731f083e04e
Nick <nthorlind@gmail.com>
2022-05-01 02:23:52 -0600

    fix hero

--- page-sections/Hero.js
+++ page-sections/Hero.js
@@ -5,7 +5,7 @@ export default function Hero() {
   return (
     <div id="hero" className={"section-header"}>
       <EaseInTransition delay={1} duration={1} initialY={50}>
-        <div className="text-title-responsive">Hi, I'm Nick</div>
+        <div className="text-title-responsive mt-10">Hi, I'm Nick</div>
         <div className="text-subtitle-responsive text-gray-500 my-6">
           I design, code, and scale data science applications
         </div>
